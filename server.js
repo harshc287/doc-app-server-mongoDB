9434d578ecc35526451bc0b380faa2f77b8b950a
harshc287 <chouhanharsh332@gmail.com>
2026-02-21 16:36:54 +0530

    cors code modified

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ dotenv.config()
 connectDB()
 
 const app = express()
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json())
-app.use(cors())
 
 app.use("/api/user", require("./routes/userRoutes"))
 app.use("/api/doctor", require("./routes/doctorRoutes"));
